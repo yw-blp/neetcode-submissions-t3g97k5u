dbb65769f31e8a44bee51e8e969250896cfc15f0
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-04-02 00:21:03 +0800

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n) {
+        let firstFiveDigits = [
+            0, 1, 1, 2, 1, 2, 2, 3,
+            1, 2, 2, 3, 2, 3, 3, 4,
+            1, 2, 2, 3, 2, 3, 3, 4,
+            2, 3, 3, 4, 3, 4, 4, 5
+        ];
+
+        function bitCounter(num) {
+            return firstFiveDigits[num >> 5] + firstFiveDigits[num & 31];
+        }
+
+        let output = firstFiveDigits.slice(0, n + 1);
+        let i = 32;
+        while (i < n + 1) {
+            output.push(bitCounter(i));
+            i++;
+        }
+
+        return output;
+    }
+}
+0
