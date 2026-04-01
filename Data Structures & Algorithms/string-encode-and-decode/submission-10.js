dafbc2f23186d409741c976e2286ce8bede0123a
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-04-01 13:27:11 +0800

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let s = [];
+        for (let i = 0; i < strs.length; i++) {
+            s.push(strs[i].length.toString().padStart(3,0) + strs[i]);
+        }
+        return s.join("");
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let strs = [];
+        let i = 0;
+        while (i < str.length) {
+            strs.push(str.substring(i + 3, i + parseInt(str.substring(i, i + 3)) + 3));
+            i += parseInt(str.substring(i, i + 3)) + 3;
+        }
+        return strs;
+    }
+}
