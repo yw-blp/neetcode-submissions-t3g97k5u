f768594669414c9831aa04adcacfe7a5ecea1a94
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-04-01 11:51:20 +0800

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        let set = new Set();
+    
+        for (let i = 0; i < nums.length; i++) {
+            if (set.has(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+
+    return false;
+    }
+}
