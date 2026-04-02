4f382687b6e6e9e26d38226c9f78b4ca12f9d453
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-04-02 12:31:12 +0800

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number} a
+     * @param {number} b
+     * @return {number}
+     */
+    getSum(a, b) {
+        function fullAdder(addend, carry) {
+            //let endValue = 4294967295;
+            //console.log(addend.toString(2), carry.toString(2));
+            if (!carry) { 
+                return addend ^ carry;
+            }
+            return fullAdder(addend ^ carry, (addend & carry) << 1);
+        }
+        return fullAdder(a, b);
+    }
+}
