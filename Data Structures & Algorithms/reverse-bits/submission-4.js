ad77b7f52ab099513561153693e644449ab5d99d
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-04-02 10:41:40 +0800

    Add: reverse-bits - submission-4

---
+++ Data Structures & Algorithms/reverse-bits/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number} - a positive integer
+     */
+    reverseBits(n) {
+
+        function binarySwap(leftIndex, rightIndex, binaryValue) {
+            let leftBit = (1 << leftIndex) >>> 0;
+            let rightBit = (1 << rightIndex) >>> 0;
+            n = (binaryValue ^ (((leftBit | rightBit) >>> 0) & ((((binaryValue & leftBit) && 1) ^ ((binaryValue & rightBit) && 1)) * 4294967295))) >>> 0;
+        }
+
+        for (let i = 0; i < 16; i++) {
+            binarySwap(31 - i, i, n);
+        }
+
+        return n;
+    }
+}
