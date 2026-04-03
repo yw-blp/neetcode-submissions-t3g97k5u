62216879973b4a183e1a905eb86f4dfcf7a66942
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-04-03 23:58:18 +0800

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        let first = 0
+        let second = 0;
+        let current = 1;
+        while (n > 0) {
+            first = second;
+            second = current;
+            current = first + second;
+            n--;
+        }
+        return current;
+    }
+}
