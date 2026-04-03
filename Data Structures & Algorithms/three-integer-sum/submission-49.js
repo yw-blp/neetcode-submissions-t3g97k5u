dea5b56798516b3e2abffea0b57eb628252d5730
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-04-03 15:27:34 +0800

    Add: three-integer-sum - submission-49

---
+++ Data Structures & Algorithms/three-integer-sum/submission-49.js
@@ -0,0 +1,91 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        let output = [];
+        nums.sort((a, b) => a - b);
+
+        function sortTriplets(array) {
+            let [a, b, c] = array;
+            if (a > b) [a, b] = [b, a];
+            if (b > c) [b, c] = [c, b];
+            if (a > b) [a, b] = [b, a];
+            return [a, b, c];
+        }
+
+        let frequencyMap = new Map();
+        for (let i = 0; i < nums.length; i++) {
+            if (!frequencyMap.has(nums[i])) {
+                frequencyMap.set(nums[i], [0]);
+            }
+            frequencyMap.get(nums[i])[0]++;
+        }
+
+        let filteredNums = [nums[0]];
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] !== nums[i - 1]) {
+                filteredNums.push(nums[i]);
+            }
+        }
+        nums = filteredNums;
+
+        let tripletSet = new Set();
+        for (let i = 0; i < nums.length; i++) {
+            let first = nums[i];
+            frequencyMap.get(first)[0]--;
+            if (frequencyMap.get(first)[0] === 0) {
+                frequencyMap.delete(first);
+            }
+            for (const second of frequencyMap.keys()) {
+                frequencyMap.get(second)[0]--;
+                let third = -(first + second);
+                if (frequencyMap.has(third) && frequencyMap.get(third)[0] > 0) {
+                    let sortedTriplet = sortTriplets([first, second, third]);
+                    if (!tripletSet.has(sortedTriplet.join(","))) {
+                        tripletSet.add(sortedTriplet.join(","))
+                        output.push(sortedTriplet);
+                    }
+                }
+                frequencyMap.get(second)[0]++;
+            }
+            if (!frequencyMap.has(first)) {
+                frequencyMap.set(first, [0]);
+            }
+            frequencyMap.get(first)[0]++;
+            //console.log(tripletSet);
+        }
+
+        return output;
+    }
+}
+
+
+
+
+
+
+        //let left = 0;
+        //let right = nums.length - 1;
+        //let target;
+        //while (left + 1 < right) {
+        //    target = -(nums[left] + nums[right]);
+        //    frequencyMap.get(nums[left])[0]--;
+        //    frequencyMap.get(nums[right])[0]--;
+        //    if (frequencyMap.get(target) > 0) {
+        //        output.push([nums[left], target, nums[right]]);
+        //    }
+        //    while (target <= nums[right]) {
+        //        frequencyMap.get(nums[left])[0]++;
+        //        right--;
+        //    }
+        //    while (target >= nums[left]) {
+        //        frequencyMap.get(nums[right])[0]++;
+        //        right--;
+        //    }
+        //    //else {
+        //    //    frequencyMap.get(nums[right])[0]++;
+        //    //    left++;
+        //    //}
+        //}
