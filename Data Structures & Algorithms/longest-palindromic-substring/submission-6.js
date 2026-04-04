e2288bbc502af289301027c47293977c38aeb628
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-04-04 15:16:11 +0800

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+        let max = [0, 0, 0];
+        let counter = [0, 0, 0];
+
+        for (let i = 0; i < s.length - 1; i++) {
+            for (let j = s.length - 1; j > i; j--) {
+                if (s[i] !== s[j]) {
+                    continue;
+                }
+                let start = i;
+                let end = j;
+                counter[0] = start;
+                counter[1] = end;
+                while (end - start > 0 && s[start] === s[end]) {
+                    counter[2] += 2;
+                    start++;
+                    end--;
+                }
+                if (end - start === 0) {
+                    counter[2]++;
+                }
+                if (end - start > 1) {
+                    counter[2] = 0;
+                }
+                if (max[2] < counter[2]) {
+                    [max, counter] = [counter, max];
+                }
+                counter[0] = 0;
+                counter[1] = 0;
+                counter[2] = 0;
+            }
+        }
+
+        return s.substring(max[0], max[1] + 1);
+    }
+}
