d0cefc13418e4f12e357e0d9936f34876be9811b
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-04-10 15:29:00 +0800

    Add: longest-palindromic-substring - submission-23

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-23.js
@@ -0,0 +1,90 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+        let max = [0, 0, 1];
+        let start = (s.length >>> 1) - 1;
+        let counter = 0;
+        for (let i = s.length >>> 1; i < s.length; i++) {
+            if (start < 0 || s[start] !== s[i]) {
+                start = i - 1;
+                counter = 0;
+            }
+            if (s[start] !== s[i]) {
+                start++;
+                continue;
+            }
+            counter += 2;
+            if (counter > max[2]) {
+                max[0] = start;
+                max[1] = i;
+                max[2] = counter;
+            }
+            start--;
+        }
+
+        start = (s.length >>> 1) - 2;
+        counter = 1;
+        for (let i = s.length >>> 1; i < s.length; i++) {
+            if (start < 0 || s[start] !== s[i]) {
+                start = i - 2;
+                counter = 1;
+            }
+            if (s[start] !== s[i]) {
+                start++;
+                continue;
+            }
+            counter += 2;
+            if (counter > max[2]) {
+                max[0] = start;
+                max[1] = i;
+                max[2] = counter;
+            }
+            start--;
+        }
+
+        start = 0;
+        counter = 0;
+        for (let i = 1; i < s.length && start < (s.length >>> 1) - 1; i++) {
+            if (start < 0 || s[start] !== s[i]) {
+                start = i - 1;
+                counter = 0;
+            }
+            if (s[start] !== s[i]) {
+                start++;
+                continue;
+            }
+            counter += 2;
+            if (counter > max[2]) {
+                max[0] = start;
+                max[1] = i;
+                max[2] = counter;
+            }
+            start--;
+        }
+
+        start = 0;
+        counter = 1;
+        for (let i = 2; i < s.length && start < (s.length >>> 1) - 2; i++) {
+            if (start < 0 || s[start] !== s[i]) {
+                start = i - 2;
+                counter = 1;
+            }
+            if (s[start] !== s[i]) {
+                start++;
+                continue;
+            }
+            counter += 2;
+            if (counter > max[2]) {
+                max[0] = start;
+                max[1] = i;
+                max[2] = counter;
+            }
+            start--;
+        }
+
+        return s.substring(max[0], max[1] + 1);
+    }
+}
