d5e1ab36a05955ea990f3b10e9245f05605248fe
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-05-19 11:09:17 +0800

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let array = [];
+        let current;
+        let counterparts = new Map([
+            ["(", ")"],
+            ["[", "]"],
+            ["{", "}"],
+            [")", true],
+            ["]", true],
+            ["}", true]
+        ]);
+        for (let i = 0; i < s.length; i++) {
+            current = counterparts.get(s[i]);
+            if (current === true) {
+                if (array.pop() != s[i]) {
+                    return false;
+                }
+            }
+            else {
+                array.push(current);
+            }
+        }
+        return (array.length == 0);
+    }
+}
