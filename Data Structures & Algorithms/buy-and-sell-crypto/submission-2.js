2bce7a357bfac9e9ff3229f4cff1ef202d16dc78
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-20 12:38:50 +0800

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        if (prices.length === 1) {
+            return 0;
+        }
+
+        let right = 0;
+        let buyPrice = +Infinity;
+        let bestProfit = 0;
+
+        while (right < prices.length - 1) {
+            if (prices[right] < buyPrice) {
+                buyPrice = prices[right];
+            }
+            while (right < prices.length - 1 && prices[right] < prices[right + 1]) {
+                right++;
+            }
+            if (prices[right] - buyPrice > bestProfit) {
+                bestProfit = prices[right] - buyPrice;
+            }
+            right++;
+        }
+
+        return bestProfit;
+    }
+}
