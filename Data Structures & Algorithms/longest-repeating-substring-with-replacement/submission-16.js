770fc687e25eb80c8072ba425ca2c43900aa0ba6
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-21 14:06:09 +0800

    Add: longest-repeating-substring-with-replacement - submission-16

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-16.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+
+        if (s.length <= 1) {
+            return s.length;
+        }
+
+        let charToIndex = new Map();
+        for (let i = 0; i < 26; i++) {
+            charToIndex.set(String.fromCharCode(65 + i), i);
+        }
+        let charFrequencies =  new Array(26).fill(0);
+
+        let left = 0;
+        let right = 0;
+        let currentLength = 0;
+        let maxLength = 0;
+        let neededReplacements = 0;
+
+        while (right < s.length) {
+            if (k < neededReplacements) {
+                charFrequencies[charToIndex.get(s[left])]--;
+                left++;
+            }
+            else {
+                charFrequencies[charToIndex.get(s[right])]++;
+                right++;
+            }
+            currentLength = right - left
+            maxLength = Math.max(currentLength, maxLength);
+
+            neededReplacements = (currentLength + 1) - Math.max(charFrequencies[charToIndex.get(s[right])] + 1, Math.max(...charFrequencies));
+        }
+
+        return maxLength;
+    }
+}
