a81f97cb590459db17db80b1e6d5ee413b4218ae
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-21 14:59:19 +0800

    Add: longest-repeating-substring-with-replacement - submission-20

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-20.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+
+        if (s.length <= 1) {
+            return s.length;
+        }
+
+        let alphabetArray = new Array(26).fill(0);
+        let maxCount = 0;
+        let index;
+
+        let left = 0;
+        let right = 0;
+        let maxLength = 0;
+        let neededReplacements = 0;
+
+        while (right < s.length) {
+            if (k < neededReplacements) {
+                alphabetArray[s.charCodeAt(left) - 65]--;
+                left++;
+            }
+            else {
+                alphabetArray[s.charCodeAt(right) - 65]++;
+                right++;
+                maxLength = Math.max(right - left, maxLength);
+            }
+            maxCount = Math.max(maxCount, alphabetArray[s.charCodeAt(right) - 65] + 1);
+            neededReplacements = ((right + 1) - left) - maxCount;
+        }
+
+        return maxLength;
+    }
+}
