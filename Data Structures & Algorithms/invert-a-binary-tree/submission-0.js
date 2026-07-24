25b00068d2ebd46966c6a64a71e12cb7a5d4d89b
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-24 23:28:00 +0800

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {
+        if (!root) {
+            return root;
+        }
+        
+        function traverse(node) {
+            let temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if (node.left) {
+                traverse(node.left);
+            }
+            if (node.right) {
+                traverse(node.right);
+            }
+        }
+
+        traverse(root);
+        
+        return root;
+    }
+}
