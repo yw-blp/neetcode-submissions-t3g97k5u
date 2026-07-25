aba5bb68d673222d8cdf104c00d0cc56f4303b4a
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-25 15:15:46 +0800

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.js
@@ -0,0 +1,87 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     */
+    isSubtree(root, subRoot) {
+
+        function isSameTree(p, q) {
+            if (!p && !q) {
+                return true;
+            }
+            if (!p || !q) {
+                return false;
+            }
+            if (p.val !== q.val) {
+                return false;
+            }
+
+            let flag = true;
+
+            function traverse(p, q) {
+                if (p.left && q.left) {
+                    if (p.left.val === q.left.val) {
+                        traverse(p.left, q.left);
+                    }
+                    else {
+                        flag = false;
+                        return;
+                    }
+                }
+                else if (p.left || q.left) {
+                    flag = false;
+                    return;
+                }
+
+                if (p.right && q.right) {
+                    if (p.right.val === q.right.val) {
+                        traverse(p.right, q.right);
+                    }
+                    else {
+                        flag = false;
+                        return;
+                    }
+                }
+                else if (p.right || q.right) {
+                    flag = false;
+                    return;
+                }
+            }
+
+            traverse(p, q);
+
+            return flag;
+        }
+
+        let isSub = false;
+        function traverse(root) {
+            if (isSameTree(root, subRoot)) {
+                isSub = true;
+                return;
+            }
+            if (root.left) {
+                traverse(root.left);
+            }
+            if (root.right) {
+                traverse(root.right);
+            }
+        }
+
+        traverse(root);
+
+        return isSub;
+
+    }
+}
