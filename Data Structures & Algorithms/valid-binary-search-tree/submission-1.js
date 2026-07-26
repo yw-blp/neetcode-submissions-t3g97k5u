ef23d6fb2f51b388a83cac8571c4d3e12bbb811d
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-26 21:42:59 +0800

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root) {
+        if (!root.left && !root.right) {
+            return true;
+        }
+
+        let flag = true;
+        function traverse(node, lowest, highest) {
+            if (!flag) {
+                return;
+            }
+            if (node.left) {
+                if (node.left.val < node.val && node.left.val > lowest) {
+                    traverse(node.left, lowest, node.val);
+                }
+                else {
+                    flag = false;
+                    return;
+                }
+            }
+            if (node.right) {
+                if (node.right.val > node.val && node.right.val < highest) {
+                    traverse(node.right, node.val, highest);
+                }
+                else {
+                    flag = false;
+                    return;
+                }
+            }
+        }
+        traverse(root, -Infinity, +Infinity);
+        
+        return flag;
+    }
+}
