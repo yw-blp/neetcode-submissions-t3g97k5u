fec62bcbb594384e3a280cb2167cbff11e6d4fc9
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-26 23:47:33 +0800

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-2.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, leftHalf = null, rightHalf = null) {
+ *         this.val = val;
+ *         this.leftHalf = leftHalf;
+ *         this.rightHalf = rightHalf;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {number[]} preorder
+     * @param {number[]} inorder
+     * @return {TreeNode}
+     */
+    buildTree(preorder, inorder) {
+        let inorderIndex = new Map();
+        for (let i = 0; i < inorder.length; i++) {
+            inorderIndex.set(inorder[i], i);
+        }
+
+        let i = -1;
+        function divide(left, right) {
+            let length = right - left;
+            if (left > right) {
+                return null;
+            }
+
+            i++;
+            let midpoint = inorderIndex.get(preorder[i]);
+            if (!(left <= midpoint && midpoint <= right)) {
+                return null;
+            }
+
+            if (length === 0) {
+                return new TreeNode(preorder[i]);
+            }
+
+            return new TreeNode(preorder[i], divide(left, midpoint - 1), divide(midpoint + 1, right));
+        }
+
+        return divide(0, inorder.length - 1);
+    }
+}
