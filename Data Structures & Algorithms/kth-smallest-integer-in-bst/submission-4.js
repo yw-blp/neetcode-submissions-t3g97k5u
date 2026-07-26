41b6e70737f8f7edc63c6dc0237c714586a6911c
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-26 21:57:54 +0800

    Add: kth-smallest-integer-in-bst - submission-4

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-4.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} k
+     * @return {number}
+     */
+    kthSmallest(root, k) {
+        let output;
+        function traverse(node) {
+            if (node) {
+                if (node.left) {
+                    traverse(node.left);
+                }
+                if (k === 0) {
+                    return;
+                }
+                output = node.val;
+                k--;
+                if (node.right) {
+                    traverse(node.right);
+                }
+            }
+        }
+        traverse(root);
+        return output;
+    }
+}
