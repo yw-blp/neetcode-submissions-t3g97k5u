de8225588ab01b261960e5f7b41c8ac4a1a25858
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-26 21:08:01 +0800

    Add: lowest-common-ancestor-in-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-2.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {TreeNode}
+     */
+    lowestCommonAncestor(root, p, q) {
+        while (true) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            }
+            else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            }
+            else {
+                return root;
+            }
+        }
+    }
+}
