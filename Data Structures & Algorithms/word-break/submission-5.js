1e1c81565343e12d0dd33073b3cc8efe3f54cdca
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-29 00:15:40 +0800

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.js
@@ -0,0 +1,74 @@
+class TrieNode {
+    constructor() {
+        this.children = new Map();
+        this.isEnd = false;
+    }
+}
+
+class PrefixTree {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let currentNode = this.root;
+        for (const char of word) {
+            if (!currentNode.children.has(char)) {
+                currentNode.children.set(char, new TrieNode());
+            }
+            currentNode = currentNode.children.get(char);
+        }
+        currentNode.isEnd = true;
+    }
+
+    searchValidWordLengths(word, start = 0) {
+        let validWordLengths = [];
+        let currentNode = this.root;
+        for (let i = start; i <= word.length; i++) {
+            if (currentNode.isEnd) {
+                validWordLengths.push(i - start);
+            }
+            if (!currentNode.children.has(word[i])) {
+                return validWordLengths;
+            }
+            currentNode = currentNode.children.get(word[i]);
+        }
+        return validWordLengths;
+    }
+}
+
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {boolean}
+     */
+    wordBreak(s, wordDict) {
+        if (s.length === 0) {
+            return true;
+        }
+
+        let validWords = new PrefixTree();
+        
+        for (const word of wordDict) {
+            validWords.insert(word);
+        }
+
+        let validWordLengths = []; // lengths of valid words in each index of s
+        for (let i = 0; i < s.length; i++) {
+            validWordLengths.push(validWords.searchValidWordLengths(s, i));
+        }
+
+        let index = 0;
+        let indexTraversalHistory = [0];
+        while (index < s.length && indexTraversalHistory.length > 0) {
+            if (validWordLengths[index].length <= 0) {
+                index = indexTraversalHistory.pop();
+                continue;
+            }
+            indexTraversalHistory.push(index);
+            index += validWordLengths[index].pop();
+        }
+        return (index === s.length);
+    }
+}
