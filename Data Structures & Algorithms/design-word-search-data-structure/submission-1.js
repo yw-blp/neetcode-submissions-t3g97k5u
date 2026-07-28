189f9584e389a55e9fab43637fe7bb147cdcb176
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-28 17:54:41 +0800

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.js
@@ -0,0 +1,49 @@
+class TrieNode {
+    constructor() {
+        this.children = new Map();
+        this.isEnd = false;
+    }
+}
+
+class WordDictionary {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    addWord(word) {
+        let currentNode = this.root;
+        for (const char of word) {
+            if (!currentNode.children.has(char)) {
+                currentNode.children.set(char, new TrieNode());
+            }
+            currentNode = currentNode.children.get(char);
+        }
+        currentNode.isEnd = true;
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word, root = this.root) {
+        let currentNode = root;
+        for (let i = 0; i < word.length; i++) {
+            if (word[i] === ".") {
+                for (const node of currentNode.children.values()) {
+                    if (this.search(word.slice(i + 1), node)) {
+                        return true;
+                    }
+                }
+            }
+            if (!currentNode.children.has(word[i])) {
+                return false;
+            }
+            currentNode = currentNode.children.get(word[i]);
+        }
+        return currentNode.isEnd;
+    }
+}
