28de10eb6aa046bf03c78e3a3b9c4f243634b3f7
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-30 18:27:56 +0800

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        nums.sort((a, b) => a - b);
+
+        let combinations = [];
+        let combination = [];
+        //let frequencies = new Array(nums.length).fill(0);
+
+        function traverse(sum, start) {
+            if (sum === target) {
+                combinations.push([...combination]);
+                return;
+            }
+            for (let i = start; i < nums.length; i++) {
+                if (sum + nums[i] > target) {
+                    break;
+                }
+                combination.push(nums[i]);
+                traverse(sum + nums[i], i);
+                combination.pop();
+            }
+        }
+        traverse(0, 0);
+
+        return combinations;
+    }
+}
