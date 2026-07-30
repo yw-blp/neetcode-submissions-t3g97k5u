f31512a85fa4178e45a59140543359d1fda7d2bb
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-07-31 01:15:08 +0800

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.js
@@ -0,0 +1,28 @@
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    constructor(k, nums) {
+        this.k = k;
+        this.minHeap = new MinPriorityQueue();
+        for (let i = 0; i < nums.length; i++) {
+            this.minHeap.enqueue(nums[i]);
+            if (this.minHeap.size() > this.k) {
+                this.minHeap.dequeue();
+            }
+        }
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val) {
+        this.minHeap.enqueue(val);
+        if (this.minHeap.size() > this.k) {
+            this.minHeap.dequeue();
+        }
+        return this.minHeap.front();
+    }
+}
