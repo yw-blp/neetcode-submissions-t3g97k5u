aabbc7b0b0c1f18e435d122aa5eb1cc2738a41e7
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-02 14:29:44 +0800

    Add: course-schedule - submission-29

---
+++ Data Structures & Algorithms/course-schedule/submission-29.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        let prerequisiteToCourses = new Map();
+        let courseToPrerequisites = new Map();
+        for (const [course, prerequisite] of prerequisites) {
+            if (!prerequisiteToCourses.has(prerequisite)) {
+                prerequisiteToCourses.set(prerequisite, new Set());
+            }
+            if (!courseToPrerequisites.has(course)) {
+                courseToPrerequisites.set(course, new Set());
+            }
+            prerequisiteToCourses.get(prerequisite).add(course);
+            courseToPrerequisites.get(course).add(prerequisite);
+        }
+
+        let coursesToDo = [];
+        for (let i = 0; i < numCourses; i++) {
+            if (!courseToPrerequisites.has(i)) {
+                coursesToDo.push(i);
+            }
+        }
+
+        for (let i = 0; i < coursesToDo.length; i++) {
+            if (!prerequisiteToCourses.has(coursesToDo[i])) {
+                continue;
+            }
+            for (const lockedCourse of prerequisiteToCourses.get(coursesToDo[i])) {
+                courseToPrerequisites.get(lockedCourse).delete(coursesToDo[i]);
+                if (courseToPrerequisites.get(lockedCourse).size <= 0) {
+                    courseToPrerequisites.delete(lockedCourse);
+                }
+                if (!courseToPrerequisites.has(lockedCourse)) {
+                    coursesToDo.push(lockedCourse);
+                }
+            }
+            prerequisiteToCourses.delete(coursesToDo[i]);
+        }
+
+        return (courseToPrerequisites.size === 0);
+    }
+}
