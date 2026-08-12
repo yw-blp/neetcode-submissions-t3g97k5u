1dcc58e7c4bd77fecb06d20caf2663d28a882d59
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-12 21:43:36 +0800

    Add: set-zeroes-in-matrix - submission-3

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-3.js
@@ -0,0 +1,63 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {void}
+     */
+    setZeroes(matrix) {
+        let n = matrix.length;
+        let m = matrix[0].length;
+
+        function setRowToZeroes(row) {
+            for (let col = 0; col < m; col++) {
+                matrix[row][col] = 0;
+            }
+        }
+        function setColToZeroes(col) {
+            for (let row = 0; row < n; row++) {
+                matrix[row][col] = 0;
+            }
+        }
+
+        let topRowHasZero = false;
+        let leftColHasZero = false;
+        for (let row = 0; row < n; row++) {
+            if (matrix[row][0] === 0) {
+                leftColHasZero = true;
+                break;
+            }
+        }
+        for (let col = 0; col < m; col++) {
+            if (matrix[0][col] === 0) {
+                topRowHasZero = true;
+                break;
+            }
+        }
+
+        for (let row = 1; row < n; row++) {
+            for (let col = 1; col < m; col++) {
+                if (matrix[row][col] === 0) {
+                    matrix[row][0] = 0;
+                    matrix[0][col] = 0;
+                }
+            }
+        }
+
+        for (let row = 1; row < n; row++) {
+            if (matrix[row][0] === 0) {
+                setRowToZeroes(row);
+            }
+        }
+        for (let col = 1; col < m; col++) {
+            if (matrix[0][col] === 0) {
+                setColToZeroes(col);
+            }
+        }
+        
+        if (topRowHasZero) {
+            setRowToZeroes(0);
+        }
+        if (leftColHasZero) {
+            setColToZeroes(0);
+        }
+    }
+}
