48b34955860bc9964e2f56b176f729f3d0c22dc8
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-12 21:02:28 +0800

    Add: spiral-matrix - submission-4

---
+++ Data Structures & Algorithms/spiral-matrix/submission-4.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {number[]}
+     */
+    spiralOrder(matrix) {
+        let n = matrix.length;
+        let m = matrix[0].length;
+        let matrixArea = n * m;
+        let spiralArray = [];
+
+        let topBorder = 0;
+        let rightBorder = m - 1;
+        let bottomBorder = n - 1;
+        let leftBorder = 0;
+
+        while (true) {
+            if (spiralArray.length >= matrixArea) {
+                return spiralArray;
+            }
+            for (let x = leftBorder; x <= rightBorder; x++) {
+                spiralArray.push(matrix[topBorder][x]);
+            }
+            topBorder++;
+
+            for (let y = topBorder; y <= bottomBorder; y++) {
+                spiralArray.push(matrix[y][rightBorder]);
+            }
+            rightBorder--;
+
+            if (spiralArray.length >= matrixArea) {
+                return spiralArray;
+            }
+            for (let x = rightBorder; x >= leftBorder; x--) {
+                spiralArray.push(matrix[bottomBorder][x]);
+            }
+            bottomBorder--;
+
+            for (let y = bottomBorder; y >= topBorder; y--) {
+                spiralArray.push(matrix[y][leftBorder]);
+            }
+            leftBorder++;
+        }
+    }
+}
