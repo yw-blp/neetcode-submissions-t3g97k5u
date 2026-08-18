87bad3a01594afe708aaef6c0be91cb1b43084e5
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-18 17:32:07 +0800

    Add: meeting-schedule-ii - submission-9

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-9.js
@@ -0,0 +1,38 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals) {
+
+        if (intervals.length === 0) {
+            return 0;
+        }
+        if (intervals.length === 1) {
+            return 1;
+        }
+
+        let sortedIntervals = [...intervals].sort((a, b) => a.start - b.start);
+        let minEnds = new MinPriorityQueue();
+
+        minEnds.enqueue(sortedIntervals[0].end);
+        for (let i = 1; i < sortedIntervals.length; i++) {
+            if (sortedIntervals[i].start >= minEnds.front()) {
+                minEnds.dequeue();
+            }
+            minEnds.enqueue(sortedIntervals[i].end);
+        }
+
+        return minEnds.size();
+    }
+}
