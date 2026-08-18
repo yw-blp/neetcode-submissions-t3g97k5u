50d2b135b2374a7cc0ffd9b8ff207a9e165b5c78
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-18 14:24:31 +0800

    Add: non-overlapping-intervals - submission-19

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-19.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+
+        intervals.sort((a, b) => a[1] - b[1]);
+
+        function hasOverlaps(arr, target) {
+            let left = 0;
+            let right = arr.length;
+            while (left < right) {
+                let mid = (left + right) >> 1;
+                if (arr[mid][1] <= target) {
+                    left = mid + 1;
+                } 
+                else {
+                    right = mid;
+                }
+            }
+            return arr.length - left > 0;
+        }
+
+        let nonOverlappingIntervals = [];
+        for (const interval of intervals) {
+            if (!hasOverlaps(nonOverlappingIntervals, interval[0])) {
+                nonOverlappingIntervals.push(interval);
+            }
+        }
+
+        return intervals.length - nonOverlappingIntervals.length;
+    }
+}
