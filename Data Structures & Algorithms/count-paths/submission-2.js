b5bbc18b773be495312f448a2127be51ef348ea7
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-19 13:00:46 +0800

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+
+        [m, n] = [Math.min(m, n) - 1, Math.max(m, n) - 1];
+
+        let i = 1;
+        let factorial = 1;
+        let totalSteps = m + n;
+        let mF;
+        let totalF;
+
+        while (i <= m) {
+            factorial *= i;
+            i++;
+        }
+        mF = factorial;
+
+        i = n + 1;
+        factorial = 1;
+        while (i <= totalSteps) {
+            factorial *= i;
+            i++;
+        }
+        totalF = factorial;
+
+        return Math.round(totalF / mF);
+    }
+}
