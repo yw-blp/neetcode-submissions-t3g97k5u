b00f2a95236584c49cf5465186fa0da96cb44b80
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-19 13:14:57 +0800

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let total = 0;
+        let highest = -Infinity;
+
+        for (let i = 0; i < nums.length; i++) {
+            highest = Math.max(nums[i], highest);
+
+            if (total + nums[i] <= 0) {
+                total = 0;
+                continue;
+            }
+            total += nums[i];
+            highest = Math.max(total, highest);
+        }
+
+        return highest;
+    }
+}
