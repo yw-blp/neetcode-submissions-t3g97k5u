aef529d11905b70b52a5b735e4689fb7706aba73
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-19 19:54:02 +0800

    Add: longest-common-subsequence - submission-6

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-6.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} text1
+     * @param {string} text2
+     * @return {number}
+     */
+    longestCommonSubsequence(text1, text2) {
+
+        if (text1.length < text2.length) {
+            [text1, text2] = [text2, text1];
+        }
+
+        const n = text1.length
+        const m = text2.length;
+        let currRowContents = new Array(m + 1).fill(0);
+
+        for (let row = 1; row <= n; row++) {
+            let upperLeftCellContent = 0;
+            for (let col = 1; col <= m; col++) {
+                let upperCellContent = currRowContents[col];
+                if (text1[row - 1] === text2[col - 1]) {
+                    currRowContents[col] = upperLeftCellContent + 1;
+                }
+                else {
+                    currRowContents[col] = Math.max(currRowContents[col - 1], currRowContents[col]);
+                }
+                upperLeftCellContent = upperCellContent;
+            }
+        }
+
+        return currRowContents[currRowContents.length - 1];
+    }
+}
