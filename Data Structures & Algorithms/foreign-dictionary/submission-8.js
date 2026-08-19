19c88af6d68422f9779933f0d2d8efd8b69395d7
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-20 00:38:04 +0800

    Add: foreign-dictionary - submission-8

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-8.js
@@ -0,0 +1,77 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @returns {string}
+     */
+    foreignDictionary(words) {
+        
+        let letterOrdering = new Map();
+        let reverseLetterOrdering = new Map();
+
+        for (let i = 1; i < words.length; i++) {
+            let letterOrder = [];
+            let minWordLength = Math.min(words[i - 1].length, words[i].length);
+            for (let j = 0; j < minWordLength; j++) {
+                if (words[i - 1][j] !== words[i][j]) {
+                    letterOrder = [words[i - 1][j], words[i][j]];
+                    break;
+                }
+            }
+            if (letterOrder.length < 2) {
+                if (words[i - 1].length > words[i].length) {
+                    return "";
+                }
+                continue;
+            }
+            if (!letterOrdering.has(letterOrder[0])) {
+                letterOrdering.set(letterOrder[0], new Set());
+            }
+            if (!reverseLetterOrdering.has(letterOrder[1])) {
+                reverseLetterOrdering.set(letterOrder[1], new Set());
+            }
+            letterOrdering.get(letterOrder[0]).add(letterOrder[1]);
+            reverseLetterOrdering.get(letterOrder[1]).add(letterOrder[0]);
+        }
+        console.log(letterOrdering);
+        console.log(reverseLetterOrdering);
+
+        let lettersWithoutPrereq = new Set();
+        let queue = [];
+        for (let i = words.length - 1; i >= 0; i--) {
+            for (let j = words[i].length - 1; j >= 0; j--) {
+                if (!reverseLetterOrdering.has(words[i][j])) {
+                    if (!lettersWithoutPrereq.has(words[i][j])) {
+                        queue.push(words[i][j]);
+                    }
+                    lettersWithoutPrereq.add(words[i][j]);
+                }
+            }
+        }
+
+        console.log(queue);
+
+        for (let i = 0; i < queue.length; i++) {
+            if (!letterOrdering.has(queue[i])) {
+                continue;
+            }
+            for (const nextLetter of letterOrdering.get(queue[i])) {
+                reverseLetterOrdering.get(nextLetter).delete(queue[i]);
+                if (reverseLetterOrdering.get(nextLetter).size <= 0) {
+                    reverseLetterOrdering.delete(nextLetter);
+                }
+                if (!reverseLetterOrdering.has(nextLetter)) {
+                    queue.push(nextLetter);
+                }
+            }
+            letterOrdering.delete(queue[i]);
+            //console.log(queue);
+        }
+
+        if (letterOrdering.size === 0) {
+            return queue.join('');
+        }
+        else {
+            return "";
+        }
+    }
+}
