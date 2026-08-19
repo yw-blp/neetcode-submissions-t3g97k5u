2c247cda58cb460c2ad7a5543b028cc46e0bf5d7
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-08-19 13:23:45 +0800

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canJump(nums) {
+
+        let i = 0;
+        let validSteps = 1;
+
+        while (i < nums.length && validSteps > 0) {
+            validSteps--;
+            validSteps = Math.max(nums[i], validSteps);
+            i++;
+        }
+
+        return i >= nums.length;
+    }
+}
